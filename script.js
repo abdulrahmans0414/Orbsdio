034564194bbbd58850a3efc4b1d57b0955a0f8ad
Abdul Rahman <abdulrahmans0414@gmail.com>
2025-01-11 12:44:40 +0530

    change style

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ menuToggle.addEventListener('click', () => {
     navLinks.classList.toggle('show');
     menuToggle.classList.toggle('open');
 });
+
+
+document.getElementById("menuToggle").addEventListener("click", function () {
+    const navLinks = document.getElementById("navLinks");
+    navLinks.style.display = navLinks.style.display === "block" ? "none" : "block";
+});
